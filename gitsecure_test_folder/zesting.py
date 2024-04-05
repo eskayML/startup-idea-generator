3d281efec8e82be7249cf06d2013b7510af07eaf
eskayML <kalusam619@gmail.com>
2024-04-05 13:57:06 +0100

    add: gitsecure

--- gitsecure_test_folder/zesting.py
+++ gitsecure_test_folder/zesting.py
@@ -11,7 +11,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.template import Template
 
 # ligma balls
-# tinubu dey gba
+# tinubu dey 
+
+# where is buhari
 subprocess.call("grep -R {} .".format(sys.argv[1]), shell=True, cwd="/home/user")
 
 def bad3(request):
