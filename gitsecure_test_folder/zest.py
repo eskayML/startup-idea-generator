49bf568ab7edf7ba7d7cbf9643ec5305bf8008e8
eskayML <kalusam619@gmail.com>
2024-04-09 15:15:29 +0100

    add: zest insecurity

--- gitsecure_test_folder/zest.py
+++ gitsecure_test_folder/zest.py
@@ -1,4 +1,4 @@
-rom django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
